2b62bec7d738e948b81512f803f508c48e7af36c
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-03 11:32:02 +0900

    Finish-Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorulMessage from "./components/ColofulMessage";
 
 const App = () => {
+  //引数は。[変数名, 関数名]にする
+  //stateは動的な値を更新する場合に使用する
+  const [num, setNum] = useState(0);
+  console.log("最初");
+  //()かっこ内は初期値
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickCountUp = () => {
     //下記、関数名を使用する
     setNum(num + 1);
   };
-  //引数は。[変数名, 関数名]にする
-  //stateは動的な値を更新する場合に使用する
-  const [num, setNum] = useState(0);
-  //()かっこ内は初期値
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       {/*外側の{}は、javascriptであることを表し、内側の{}はjavascriptの関数を書く際の{}*/}
@@ -19,7 +34,7 @@ const App = () => {
       <ColorulMessage color="pink" size="36px" message="元気です" />
       <button onClick={onClickCountUp}>カウントアップ!</button>
       <br />
-      <button>on/off</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
       {faceShowFlag && <p>(^o^)</p>}
     </>
